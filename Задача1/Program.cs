96ec2ad4a971e7c5210de5b92aab2527c7bc03b8
Nata <muravevantsh@mail.ru>
2022-08-07 00:47:38 +0300

    задача 1

--- Задача1/Program.cs
+++ Задача1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = new Random ().Next (1,10);
+Console.WriteLine (a);
+int b = new Random ().Next (1,10);
+Console.WriteLine (b);
+Console.WriteLine ("Квадрат второго числа равен: ");
+Console.WriteLine (b*b);
+if (a==b*b)
+{
+Console.WriteLine ("Первое число - это квадрат второго числа: ");
+}
