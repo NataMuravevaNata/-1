d6b4f3cf2d9d318d53a90de1871cfaa0cc5ad693
Nata <muravevantsh@mail.ru>
2022-08-07 11:24:27 +0300

    задачи 2,3

---
+++ Задача1/Задача2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число (от 1 до 7) ");
+string username = Console.ReadLine();
+if (username == "1")
+{
+    Console.WriteLine ("Понедельник");
+}    
+if (username == "2")
+{
+    Console.WriteLine ("Вторник");
+}  
+if (username == "3")
+{
+    Console.WriteLine ("Среда");
+} 
+ if (username == "4")
+{
+    Console.WriteLine ("Четверг");
+}   
+ if (username == "5")
+{
+    Console.WriteLine ("Пятница");
+}    
+    if (username == "6")
+{
+    Console.WriteLine ("Суббота");
+}  
+  if (username == "7")
+{
+    Console.WriteLine ("Воскресенье");
+}    
---
+++ Задача1/Задача3/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = 1;
+int b = 5;
+int c = 12;
+int max=a;
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.Write ("max= " );
+Console.WriteLine (max);
